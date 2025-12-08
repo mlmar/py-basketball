97fb80dfb3c0b18b3dc8e2e586753584439dda2a
mlmar <marcloussm@gmail.com>
2025-12-08 10:32:52 -0800

    Updating client/server urls to be pulled from .env file

--- src/client/src/services/Config.ts
+++ src/client/src/services/Config.ts
@@ -1,3 +1,3 @@
 export const Config = {
-    SERVER_URL: import.meta.env.DEV ? 'http://localhost:3300/api' : 'https://waiverwarrior.vercel.app/api'
+    SERVER_URL: import.meta.env.VITE_SERVER_URL
 }
--- src/server/config.py
+++ src/server/config.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DEV: str = os.environ.get('DEV')
+CLIENT_URL: str = os.environ.get('VITE_CLIENT_URL')
+SERVER_URL: str = os.environ.get('VITE_SERVER_URL')
 SUPABASE_URL: str = os.environ.get('SUPABASE_URL')
 SUPABASE_KEY: str = os.environ.get('SUPABASE_KEY')
 SUPABASE_JWT_SECRET: str = os.environ.get('SUPABASE_JWT_SECRET')
